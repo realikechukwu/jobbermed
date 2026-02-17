c3412130536073b5a3f2fe376b82b33c3e1d8b62
Ikechukwu Chukwudi <ikechukwu.chukwudi@gmail.com>
2026-02-17 21:32:01 +0000

    correct empty page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,9 +6,11 @@ import { SessionProvider } from "./features/auth/session-context";
 import "./styles/tokens.css";
 import "./styles/global.css";
 
+const routerBase = import.meta.env.BASE_URL === "/" ? "/" : import.meta.env.BASE_URL.replace(/\/$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBase}>
       <SessionProvider>
         <App />
       </SessionProvider>
